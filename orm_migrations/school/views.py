17dde1633234cd0d7d1e6977b96ac42d2c2be401
Степан Крылов <50091565+stepankrylov@users.noreply.github.com>
2019-11-04 15:33:16 +0500

    Add files via upload

---
+++ orm_migrations/school/views.py
@@ -0,0 +1,13 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Student
+
+
+def students_list(request):
+    template = 'school/students_list.html'
+    ordering = 'group'
+    x = Student.objects.all().order_by(ordering)
+    context = {'object_list': x}
+
+    return render(request, template, context)
